d0423f3f7912547acb633b84f5d405d8013e8db0
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-29 11:36:18 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
+// Criando a instância do axios
 const api = axios.create({
-  baseURL: "https://estoque-springboot-production.up.railway.app/api",
+  baseURL: "https://estoque-springboot-production.up.railway.app/api", // URL base do seu backend
 });
-
 
 export default api;
